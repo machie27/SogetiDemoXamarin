cd269bb605773b8e81aa33111e1040cdc90616ce
Stephen Machwirth <steph@DESKTOP-D71J2EV>
2016-09-22 14:09:27 +0200

    initial demo datapages

---
+++ DemoDataPages/DemoDataPages/DemoDataPages/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoDataPages.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
