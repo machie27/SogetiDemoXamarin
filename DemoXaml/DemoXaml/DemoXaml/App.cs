80fded60fccf21bbf6be6859470ea8b886c9a8e0
Stephen Machwirth <steph@DESKTOP-D71J2EV>
2016-09-22 11:05:26 +0200

    first demo project initial

---
+++ DemoXaml/DemoXaml/DemoXaml/App.cs
@@ -0,0 +1,53 @@
+﻿using DemoXaml.ViewModels;
+using DemoXaml.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace DemoXaml
+{
+    public class App : Application
+    {
+
+        private static ViewModelLocator _locator;
+
+        public static ViewModelLocator Locator
+        {
+            get
+            {
+                return _locator ?? (_locator = new ViewModelLocator());
+            }
+        }
+
+
+        public static Page GetMainPage()
+        {
+            return new MainPage();
+        }
+        public App()
+        {
+            // The root page of your application
+
+
+            MainPage = GetMainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ DemoXaml/DemoXaml/DemoXaml/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DemoXaml.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel(INavigation navigation)
+        {
+
+        }
+    }
+}
